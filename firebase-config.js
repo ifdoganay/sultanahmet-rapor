c57077aeaf3b8cb00c46df44ae191599248e8770
ifdoganay <ifdoganay@gmail.com>
2026-04-29 11:54:17 +0300

    Upload firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+// Firebase Yapılandırması - sultanahmet-rapor projesi
+firebase.initializeApp({
+    apiKey:            "AIzaSyBfizkWrmlnFmGwv23QC8KBAB6o9pSKhRs",
+    authDomain:        "sultanahmet-rapor.firebaseapp.com",
+    projectId:         "sultanahmet-rapor",
+    storageBucket:     "sultanahmet-rapor.firebasestorage.app",
+    messagingSenderId: "215230302444",
+    appId:             "1:215230302444:web:5359290a34e883739d7dc9"
+});
+
+const db = firebase.firestore();
